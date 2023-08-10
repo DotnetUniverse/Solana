8bf9e156c24f51127f5837f2c65f2e7f3c890d8c
trandat1114 <cctalk1114@gmail.com>
2023-08-10 10:00:43 +0700

    ok

--- bapcai_hackday/src/components/layout/layout.tsx
+++ bapcai_hackday/src/components/layout/layout.tsx
@@ -25,22 +25,23 @@ export const Layout = ({ children }: PropsWithChildren) => {
     const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter()], [network])
 
     return (
-        <div className="p-4 min-h-full w-full">
+        <div className="min-h-full w-full">
             <ConnectionProvider endpoint={endpoint}>
                 <WalletProvider wallets={wallets} autoConnect>
                     <WalletModalProvider>
-                        <header className="flex flex-row justify-between items-center sticky ">
+                        <header className="flex flex-row justify-between items-center sticky w-full  top-0 z-50 border-b backdrop-blur p-4">
                             <div className="flex flex-row justify-between items-center gap-4">
-<h1 className="text-2xl text-green-500 text-whitespace-nowrap">B A P C A I</h1>
-                            <NavigationMenuHeader />
+                                <h1 className="text-2xl text-green-500 text-whitespace-nowrap">B A P C A I</h1>
+                                <NavigationMenuHeader />
                             </div>
                             <div className="flex flex-row justify-center items-center">
                                 <ConnectWalletButton />
                                 <ModeToggle />
                             </div>
                         </header>
-                        <main className="flex justify-center py-4">                            
-                                {children}
+
+                        <main className="flex justify-center py-4 min-h-screen">
+                            {children}
                         </main>
                     </WalletModalProvider>
                 </WalletProvider>
