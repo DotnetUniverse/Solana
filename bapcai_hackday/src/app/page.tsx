6aa40b5abc62f106872a69da185e186fcf14fd36
trandat1114 <cctalk1114@gmail.com>
2023-08-10 11:19:38 +0700

    okela

--- bapcai_hackday/src/app/page.tsx
+++ bapcai_hackday/src/app/page.tsx
@@ -4,11 +4,12 @@ export default function Home() {
   return (
     <div className='max-h-'>
 
-    <div className='grid grid-rows-3'>
-      <h1 className='row-span-1 text-4xl md:text-9xl text-green-500 uppercase'>Say Hi to SOLANA HACKATHON 05</h1>
+      <div className='grid grid-rows-3'>
+        <p className='row-span-1 text-4xl md:text-8xl text-green-500 uppercase'>BAP CAI HACKATHON Say Hi to SOLANA HACKATHON 05</p>
 
-      <Image className='row-span-2 w-full object-contain' src={bapcai} alt="Bapcai" />
-    </div>
+
+        <Image className='row-span-2 w-full object-contain' src={bapcai} alt="Bapcai" />
+      </div>
     </div>
 
 
