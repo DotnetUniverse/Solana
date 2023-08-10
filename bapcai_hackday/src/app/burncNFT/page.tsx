b8ec2be854641ed44c6466083db0938a5221d7ae
Trung_test <nrong157214@gmail.com>
2023-08-10 14:41:39 +0700

    trung

--- bapcai_hackday/src/app/burncNFT/page.tsx
+++ bapcai_hackday/src/app/burncNFT/page.tsx
@@ -1,3 +1,13 @@
+import { BurnNFTForm } from "@/components/form/burn-nft";
+
 export default function Page() {
-      return <h1>Hello, Next.js!</h1>;
+      return(<>
+        <div className="mb-10">
+          <p className="mb-2 font-bold 2xl">
+            Burn cNFT
+          </p>
+        </div>
+        <BurnNFTForm />
+      </>) 
+      ;
     }
